233c0771cf8c6ccdcefa247642454f5e72b4dc8d
myanikaislam1920@gmail.com <myanikaislam1920@gmail.com>
2025-11-08 18:19:50 +0600

    added client side

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import CarDetails from "./pages/CarDetails";
+import Cars from "./pages/Cars";
+import MyBookings from "./pages/MyBookings";
+import Footer from "./components/Footer";
+import Layout from "./pages/owner/Layout";
+import Dashboard from "./pages/owner/Dashboard";
+import AddCar from "./pages/owner/AddCar";
+import ManageCars from "./pages/owner/ManageCars";
+import ManageBookings from "./pages/owner/ManageBookings";
+import Login from "./components/Login";
+
+const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
+  const isOwnerPath = useLocation().pathname.startsWith("/owner");
+
+  return (
+    <>
+      {showLogin && <Login setShowLogin={setShowLogin}></Login>}
+
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/car-details/:id" element={<CarDetails />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/owner" element={<Layout></Layout>}>
+          <Route index element={<Dashboard></Dashboard>} />
+          <Route path="add-car" element={<AddCar></AddCar>} />
+          <Route path="manage-cars" element={<ManageCars></ManageCars>} />
+          <Route
+            path="manage-bookings"
+            element={<ManageBookings></ManageBookings>}
+          />
+        </Route>
+      </Routes>
+      {!isOwnerPath && <Footer></Footer>}
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/owner/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavbarOwner from '../../components/owner/NavbarOwner';
+import Sidebar from '../../components/owner/Sidebar';
+import { Outlet } from 'react-router-dom';
+
+const Layout = () => {
+    return (
+        <div className='flex flex-col'>
+            <NavbarOwner></NavbarOwner>
+            <div className='flex'>
+                <Sidebar></Sidebar>
+                <Outlet></Outlet>
+            </div>
+        </div>
+    );
+};
+
+export default Layout;
